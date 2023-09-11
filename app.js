7dcddf7dce1d68eec6d164def638052de640c455
Holland28 <137765368+Holland28@users.noreply.github.com>
2023-09-11 08:32:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ DG.then(function () {
 			blink(timeChoise)
 		})
 		.addTo(map)
-		.bindPopup(`<p>${shops['БАУ'][1])}<br />${shops['БАУ'][4])}</p>`)
+		.bindPopup(`${shops['БАУ'][1]}\n ${shops['БАУ'][4]}`)
 			
 			   
 /*
